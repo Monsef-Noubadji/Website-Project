d2871e603fca946df0e75dfa9a102bcab6672105
Monsef Noubadji <67560520+Monsef-Noubadji@users.noreply.github.com>
2021-07-01 12:43:05 +0100

    Added About us & categories sections

---
+++ Website/about-us.php
@@ -0,0 +1,75 @@
+<html>
+
+<head>
+    <link rel="stylesheet" href="/styles/about-us.css">
+    <meta name="viewport" content="width=device-width,initial-scale=1.0">
+</head>
+
+<body>
+    <nav>
+        <?php
+        include "nav.php"
+        ?>
+    </nav>
+    <div class ="about">
+        <div class = "sp">
+
+        </div>
+        <div class = "acontainer">
+            <div class = "sp">
+            
+            </div>
+            <div class="atitle">
+                <p>About Us</p>
+            </div>
+            <div class="des">
+                <p>Tech community started as an idea from a university residence , a 4 passionate Students launched the project localy first, then it gots expanded Wordlwide to reach every technology Geek around the digital globe.Today,tech communtiy still the same tiny club from the first day & will continue to be , but only the different is that it's now open source so anyone can contribute to this project to enhance it with many features. 
+                </p>
+            </div>
+        </div>
+        <div class="members">
+            <div class="mtitle">
+            <p> Our Members <p>
+            </div>
+            <div class="member">
+                <div class="mname">
+                    <p>Monsef Noubadji</p>
+                </div>
+                <div class="mdes">
+                    <p>The co-founder of the tech community & The multitasker in the team. I can combine both the Creative Design and the long lines of code to give you the right-on road Web app that you need ;)
+                    </p>
+                </div>
+            </div>
+            <div class="member">
+                <div class="mname">
+                    <p>Yousfi Yanis</p>
+                </div>
+                <div class="mdes">
+                    <p>Hello , i'm a software engineer student & a computer science Geek.i write long lines of code & solve bloomign bugs,i'll handle it as long it's not the Design part (you get it , CSS) :|
+                    </p>
+                </div>
+            </div>    
+            <div class="member">
+                <div class="mname">
+                    <p>Dellihr Mohammed</p>
+                </div>
+                <div class="mdes">
+                    <p>I'm the second software engineer in the team,I'll be happy mking things function as they should, i hte design as well :( ;
+                </div>
+            </div>
+            <div class="member">
+                <div class="mname">
+                    <p>Belblidia Yasmine</p>
+                </div>
+                <div class="mdes">
+                    <p>Hiii , I'm the Designer here, don't listen to my teammates,The Design is all about making beautiful experience through colors & emotions.That needs a creative & shiny mindset :)  
+                    </p>
+                </div>
+            </div>
+        </div>
+    </div>
+    <?php
+        include "footer.html";
+        ?>
+    </body>
+</html>
---
+++ Website/categories.php
@@ -0,0 +1,63 @@
+<html>
+<head>
+    <link rel="stylesheet" href="/styles/categories.css">
+    <script src="https://kit.fontawesome.com/a076d05399.js" crossorigin="anonymous"></script>
+    <meta name="viewport" content="width=device-width,initial-scale=1.0">
+</head>
+
+<body>
+    <nav>
+    <?php
+      include"nav.php";
+    ?>
+    </nav>
+    <div class="categories">
+
+        <div class="cat">
+            <img class="cimg" src="/images/robot.jpg" alt="robot">
+            <div class="cimg-overlay">
+                <div class="cimg-title">ROBOTS</div>
+            </div>
+        </div>
+
+        <div class="cat">
+            <img class="cimg" src="/images/computer.jpg" alt="computer">
+            <div class="cimg-overlay">
+                <div class="cimg-title">COMPUTER</div>
+            </div>
+        </div>
+
+        <div class="cat">
+            <img class="cimg" src="/images/programming.png" alt="computer">
+            <div class="cimg-overlay">
+                <div class="cimg-title">PROGRAMMING</div>
+            </div>
+        </div>
+
+        <div class="cat">
+            <img class="cimg" src="/images/cybersecurity.png" alt="cybersecurity">
+            <div class="cimg-overlay">
+                <div class="cimg-title">CYBERSECURTIY</div>
+            </div>
+        </div>
+
+        <div class="cat">
+            <img class="cimg" src="/images/mobile.jpg" alt="computer">
+            <div class="cimg-overlay">
+                <div class="cimg-title">MOBILE</div>
+            </div>
+        </div>
+
+        <div class="cat">
+            <img class="cimg" src="/images/esports.jpg" alt="cybersecurity">
+            <div class="cimg-overlay">
+                <div class="cimg-title">E-SPORTS</div>
+            </div>
+        </div>
+            <?php
+            include"footer.html"
+            ?>
+    </div>
+</body>
+
+</html>
